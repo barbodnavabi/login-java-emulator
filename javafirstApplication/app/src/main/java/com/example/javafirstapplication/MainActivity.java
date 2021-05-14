ecc368c47fa3949d47d734bf6ad408557806f490
barbod <barbodnvb@gmail.com>
2021-05-14 16:36:03 +0430

    first commit

---
+++ javafirstApplication/app/src/main/java/com/example/javafirstapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.javafirstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button buttonOne = (Button) findViewById(R.id.mybtn);
+        buttonOne.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "تو ثبت نام کردی", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
